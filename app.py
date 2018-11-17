fd78fbe976daeef49c9df16962280b006a1c49bb
Gustavo Monteiro <dinizmonteirogustavo@gmail.com>
2018-11-17 12:24:01 -0300

    Adding cors config and fix syntax error.

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
+from flask_cors import CORS
 from middleware import resend
-
 from flask import Flask, request
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def hello(path):
     if request.method == 'POST':
-        continue
+        pass
     return resend(request, path)
--- middleware.py
+++ middleware.py
@@ -2,8 +2,7 @@ import requests
 
 __all__ = ['resend']
 
-API = 'http://localhost'
+API = 'http://localhost:3000/'
 
 def resend(request, path):
-    return requests.request((request.method, ''.join(API, path),
-                     data=request.data, headers=request.headers))
+    return requests.request(request.method, API+path, data=request.data, headers=request.headers)
