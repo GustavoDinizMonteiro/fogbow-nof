855aecd745afb2925acd7dfe90d98413b90c7872
Gustavo Monteiro <dinizmonteirogustavo@gmail.com>
2018-11-17 11:28:12 -0300

    Create a catch-all app.

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from middleware import SimpleMiddleWare
 app = Flask(__name__)
+# app.wsgi_app = SimpleMiddleWare(app.wsgi_app)
 
-@app.route("/")
-def hello():
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def hello(path):
     return "Hello World!"
