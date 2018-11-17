f2f9affe4744c509a3fca7f711ff5e716410b6ec
Gustavo Monteiro <dinizmonteirogustavo@gmail.com>
2018-11-17 12:37:57 -0300

    Add detenv config.

--- middleware.py
+++ middleware.py
@@ -1,8 +1,12 @@
+import os
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API = os.getenv('API')
 
 __all__ = ['resend']
-
-API = 'http://localhost:3000/'
 
 def resend(request, path):
     return requests.request(request.method, API+path, data=request.data, headers=request.headers)
