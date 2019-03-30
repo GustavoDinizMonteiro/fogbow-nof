44494b25c90636499979c13ef4627f17276c1ad5
Gustavo Diniz Monteiro <dinizmonteirogustavo@gmail.com>
2019-03-30 18:13:49 -0300

    Implement create local algorithm.

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-import requests
 import nof_services
 from flask_cors import CORS
 from dotenv import load_dotenv
@@ -9,9 +8,6 @@ from db import create_member, update_global
 load_dotenv()
 update_global(1) # TODO: check how is the default value for this.
 
-API = os.getenv('PROVIDER_API')
-ENV = os.getenv('ENV')
-
 app = Flask(__name__)
 CORS(app)
 
@@ -20,21 +16,13 @@ ALL_METHODS = ['POST', 'PUT', 'PATCH', 'DELETE', 'GET']
 @app.route('/', defaults={'path': ''}, methods=ALL_METHODS)
 @app.route('/<path:path>', methods=ALL_METHODS)
 def catch_all(path):
+    def extract(resp):
+        return (resp.content, resp.status_code, resp.headers.items())
+
     if request.method == 'POST':
-        nof_services.create_local(request, path)
-    resp = resend(request, path)
-    return (resp.content, resp.status_code, resp.headers.items())
+        return extract(nof_services.create_local(request, path))
+    return extract(nof_services.resend(request, path))
 
-
-def resend(request, path):
-    # NOTE: send headers crash requests in Postman mock API
-    headers = {} if ENV == 'dev' else request.headers
-    return requests.request(
-        request.method, 
-        API+path, 
-        data=request.data, 
-        headers=headers, 
-        allow_redirects=True)
 
 if __name__ == '__main__':
     app.run(debug=ENV=='dev')
--- nof_services.py
+++ nof_services.py
@@ -1,8 +1,67 @@
+import os
+import requests
+from dotenv import load_dotenv
 from implementations import implementation
 
+load_dotenv()
+ENV = os.getenv('ENV')
+API = os.getenv('PROVIDER_API')
+LOCAL_MEMBER = os.getenv('NOF_MEMBER')
+
+resource_cannot_be_providede_response = {
+    'content': 'It was not possible to fulfill your request.',
+    'status_code': 404
+}
+
+
+def get_requester_from_req(request):
+    return implementation.get_requester_from_req(request)
+
+def resend(request, path):
+    # NOTE: send headers crash requests in Postman mock API
+    headers = {} if ENV == 'dev' else request.headers
+    return requests.request(
+        request.method, 
+        API+path, 
+        data=request.data, 
+        headers=headers, 
+        allow_redirects=True)
+
+def member_has_quota(member):
+    pass
+
+def get_members_with_less_quota(member):
+    pass
+
+def get_current_orders_from_member(member):
+    pass
+
+def preempt_order(order):
+    pass
+
 def create_local(request, path):
-    return implementation.create_local(request, path)
+    requester = get_requester_from_req(request)
+    if requester == LOCAL_MEMBER:
+        return resend(request, path)
+    has_quota = member_has_quota(requester)
+    if not has_quota:
+        return resource_cannot_be_providede_response
+    create_local_resp = implementation.create_local(request, path)
+    if create_local_resp.status_code < 400:
+        return create_local_resp
+    members = get_members_with_less_quota(requester)
+    for member in members:
+        orders = get_current_orders_from_member(member)
+        for order in orders:
+            preempt_order(order)
+            create_local_resp = implementation.create_local(request, path)
+            if create_local_resp.status_code < 400:
+                return create_local_resp
+    return resource_cannot_be_providede_response
+    
+    
+    
+    
 
 
-def create_remote(request, path):
-    return implementation.create_remote(request, path)
+
